ba86219932b0353f9626074fb494325fc0dff778
Kowshika K <36320336+kowshi01@users.noreply.github.com>
2023-02-23 03:35:02 +0530

    Intro to Middlewares

--- nodepro.js
+++ nodepro.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("middleware1");
-    next();
+const bodyParse=require('body-parser');
+app.use(bodyParse.urlencoded({extended :false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="post"><input type="text" name="title"> <input type="number" name="size"> <button type="submit">Add Product</button></form>');
 })
-app.use((req,res,next)=>{
-    console.log("middleware2");
-    res.send('<h1> hello to node js </h1>') 
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello Express js</h1>');
 })
 app.listen(4000);
